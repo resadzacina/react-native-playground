02ede93aa291838834c471067ddbab946c4c6cd4
Resad Zacina <resad.zacina@gmail.com>
2015-12-06 00:38:53 +0100

    [Login] login against github and handle results in simple control

--- playground/Login.js
+++ playground/Login.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react-native');
+var buffer = require('buffer');
 
 var {
   AppRegistry,
@@ -27,17 +28,54 @@ class Login extends Component {
     console.log('logging in');
     this.setState({showProgress: true});
 
-    fetch('https://api.github.com/search/repositories?q=react')
+    var b = new buffer.Buffer(this.state.username + ':' + this.state.password);
+    var encodedAuth = b.toString('base64');
+
+    fetch('https://api.github.com/user', {
+      headers: {
+        'Authorization': 'Basic ' + encodedAuth
+      }
+    })
     .then((response) => {
-        return response.json();
-      })
+      if(response.status >= 200 && response.status < 300){
+        return response;
+      }
+
+      throw {
+        badCredentials: response.status == 401,
+        unknownError: response.status != 401
+      }
+    })
+    .then((response) => {
+      return response.json();
+    })
     .then((results) => {
-        console.log(results);
-        this.setState({showProgress: false});
-      });
+      console.log(results);
+      this.setState({success: true})
+    })
+    .catch((err)=> {
+      this.setState(err);
+    })
+    .finally(()=> {
+      this.setState({showProgress: false});
+    });
   }
 
   render(){
+   var errorCtrl = <View />;
+
+   if(!this.state.success && this.state.badCredentials){
+     errorCtrl = <Text style={styles.error}>
+       That username and password combination did not work.
+     </Text>
+   }
+
+    if(!this.state.success && this.state.unknownError){
+      errorCtrl = <Text style={styles.error}>
+        We experienced an unexpected issue
+      </Text>
+    }
+
    return (
     <View style={styles.container}>
        <Image style={styles.logo}
@@ -47,7 +85,7 @@ class Login extends Component {
          Github browser
        </Text>
        <TextInput
-          onChangeText={(text)=> console.log('da')}
+          onChangeText={(text)=> this.setState({username: text})}
           style={styles.input}
           placeholder="Github username"
          />
@@ -63,6 +101,9 @@ class Login extends Component {
               Log in
            </Text>
        </TouchableHighlight>
+
+      {errorCtrl}
+
        <ActivityIndicatorIOS
           animating={this.state.showProgress}
           size="large"
@@ -113,6 +154,10 @@ var styles = StyleSheet.create({
   },
   loader: {
     marginTop: 20
+  },
+  error: {
+    color: 'red',
+    paddingTop: 10
   }
 
 });
